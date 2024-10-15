afbb0924093a350fe3ba6d479f7130271a83a733
nehha0 <neehhaa0@gmail.com>
2024-10-15 21:12:49 +0530

    check

--- app/api/auth/callback/github/route.tsx
+++ app/api/auth/callback/github/route.tsx
@@ -46,7 +46,7 @@ export async function GET(req: NextRequest){
 
 
     if(!data){
-        throw new Error('No user data provided');
+        throw new Error('No user data provided , Some Issue occured');
         return;
     }
     
