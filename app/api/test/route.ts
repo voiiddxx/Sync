2bc2268ca0f86ffec4dcf49d410bcff0fbba61d2
voiiddxx <ursfan8102003@gmail.com>
2024-11-01 09:45:10 +0530

    :)

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -3,17 +3,26 @@ import simpleGit from 'simple-git';
 import path from 'path';
 
 export async function POST(req: NextRequest) {
+
+    console.log("Api called");
+    
     // Use path.join for cross-platform compatibility
     const repoPath = path.join('E:', 'dummy', 'Echo');
     const git = simpleGit(repoPath); // Specify your local repo path
 
     try {
+        
+        console.log("Branch getting");
         const gitBranch = await git.branch(); // Await the promise
         const currentBranch = gitBranch.current;
 
+        
         console.log(currentBranch);
+
+        console.log("Yha nahi pohocha");
         
         return NextResponse.json({ message: "Done", branch: currentBranch }); // Return JSON response
+
     } catch (error) {
         console.error("Error fetching branch:", error);
         return NextResponse.json({ error: "Failed to get current branch." }, { status: 500 });
