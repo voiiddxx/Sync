8125d3351b4083e0c8ffa10d94f67d7b1ba70d0c
voiiddxx <ursfan8102003@gmail.com>
2024-11-12 20:23:09 +0530

    landing page svg added

--- components/shared/landingPage/firstFold.tsx
+++ components/shared/landingPage/firstFold.tsx
@@ -1,53 +1,31 @@
-import { GlobeDemo } from '@/components/ui/GitGlobe';
-import { ChevronRight } from 'lucide-react'
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react'
+import { GlobeDemo } from "@/components/ui/GitGlobe";
+import { ChevronRight } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import GradientButton from "../gradient-button";
 
 const FirstFold = () => {
-
-
   const GITHUB_AUTH_URL = `https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_GITHUB_REDIRECT_URL}&scope=repo,workflow,admin:repo_hook,user`;
 
   return (
-    < >
-    <div className='' >
-    <div className='h-[90vh] w-full flex flex-col justify-center items-center gap-2 dark:bg-[#050505] dark:text-white' >
-      <div className=' bg-zinc-100 px-1 py-1 rounded-full flex items-center group justify-center md:mt-20  cursor-pointer' >
-        <div className='bg-zinc-600 text-white rounded-full px-2' >
-          <p className='text-sm' >New</p>
+    <>
+      <div className="h-[600px] overflow-hidden w-full border-b relative">
+        <div className=" h-full w-full relative z-50" >
+        <h1 className="text-white/80 text-4xl font-Poppins  font-medium  " >Automate Your Workflow</h1>
+        <h1 className="text-white/80 text-4xl font-Poppins  font-medium  " >With Flow</h1>
+        <p className="text-white/50 font-Poppins font-normal mt-6" >Fumadocs is a beautiful documentation framework with a complete toolchain, <br /> powered by Next.js App Router. Designed to be flexible and fast.</p>
+
+        <div className=" flex" >
+          
         </div>
-      <p className='text-sm text-zinc-700 font-medium ml-2' > PR Scheduler on the way of launch 🎉</p>
+        </div>
       <div>
-        <ChevronRight className='group-hover:scale-125 transition-all'  strokeWidth={1.25} size={16} />
+        <Image className="w-auto absolute  bottom-0 z-10 " src={`/bg.svg`} height={1500} width={1500} alt="image" />
       </div>
       </div>
-
-    <div className='px-36' >
-    <p className='text-[100px] text-center font-sans text-[#1C1D1F] dark:text-white tracking-[-3.6px]' >Automate</p>
-    <p className='text-[100px] text-center font-sans text-[#1C1D1F] -mt-14 dark:text-white tracking-[-3.6px]' >your workflows</p>
-    <p className='text-[22px]  font-Poppins  text-zinc-700 dark:text-zinc-500 text-center ' >No more late-night commits or missed PR deadlines! Our platform <br />lets you schedule GitHub commits, pull requests 
-    </p>
-
-    <div className=' flex gap-2 z-50 w-full items-center justify-center mt-8' >
-     <Link href={GITHUB_AUTH_URL}>
-     <div className='px-6 py-3 rounded-xl flex items-center justify-center bg-gradient-to-b from-zinc-800 shadow-lg to-zinc-700 text-white cursor-pointer' >
-      <p className='font-mono font-medium' >Sign in with github</p>
-      </div></Link>
-      <div className='px-6 py-3 rounded-xl flex items-center justify-center border border-zinc-400 text-zinc-800 cursor-pointer' >
-      <p className='font-mono font-semibold' >Star on github</p>
-      </div>
-    </div>
-    {/* <Image className='' src={`/nodata.png`} height={1500} width={1500} alt='image'/> */}
-    </div>
-    </div>
-    <div className='z-20 mt-52' >
-    {/* <GlobeDemo/> */}
-    </div>
-
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default FirstFold
+export default FirstFold;
--- components/shared/landingPage/landingPage.tsx
+++ components/shared/landingPage/landingPage.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import React from "react";
 import NavBar from "./navbar";
@@ -6,21 +6,21 @@ import FirstFold from "./firstFold";
 import { useSelector } from "react-redux";
 
 const LandingPage = () => {
+  const darkmode = useSelector((state: any) => state.window.darkMode);
+  return (
+    <div className={`${darkmode ? "dark" : ""}`}>
+      <div className=" w-full min-h-screen h-screen bg-[#050505] dark:bg-[#050505] ">
+        <div className=" w-full sticky top-0">
+          <NavBar />
+        </div>
 
-  const darkmode = useSelector((state:any)=>state.window.darkMode);
-   return (
-   <div className={`${darkmode ? "dark" : ""}`} >
-      
-    <div className=" w-full min-h-screen bg-white dark:bg-[#050505]">
-      <div className=" w-full sticky top-0">
-        <NavBar />
-      </div>
-
-      <div className=" h-full w-full bg-white" >
-    <FirstFold/>
+        <div className=" w-auto px-40 mt-10 relative" >
+          <div className=" w-auto h-[900px] border relative ">
+            <FirstFold />
+          </div>
+        </div>
       </div>
     </div>
-  </div>
   );
 };
 
