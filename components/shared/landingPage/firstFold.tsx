41c8da03153545853ddf1d0b0afb7709b59b1d36
voiiddxx <ursfan8102003@gmail.com>
2024-11-12 23:01:47 +0530

    landing page

--- components/shared/landingPage/firstFold.tsx
+++ components/shared/landingPage/firstFold.tsx
@@ -10,19 +10,51 @@ const FirstFold = () => {
 
   return (
     <>
-      <div className="h-[600px] overflow-hidden w-full border-b relative">
-        <div className=" h-full w-full relative z-50" >
-        <h1 className="text-white/80 text-4xl font-Poppins  font-medium  " >Automate Your Workflow</h1>
-        <h1 className="text-white/80 text-4xl font-Poppins  font-medium  " >With Flow</h1>
-        <p className="text-white/50 font-Poppins font-normal mt-6" >Fumadocs is a beautiful documentation framework with a complete toolchain, <br /> powered by Next.js App Router. Designed to be flexible and fast.</p>
+      <div className="h-[700px] overflow-hidden w-full border-b relative">
+        <div className=" h-full w-full relative z-50 p-16">
+          <h1 className="text-white/80 text-4xl font-mono  font-medium  ">
+            Automate your github workflows{" "}
+          </h1>
+          <h1 className="text-white/80 text-4xl mt-1 font-mono  font-medium  ">
+            with less effort
+          </h1>
+          <p className="text-white/50 font-Poppins text-[18px] font-normal  mt-8">
+            Boost your productivity on GitHub with the floww seamless,
+            user-friendly tool for developers
+            <br /> Whether you're scheduling issues
+          </p>
 
-        <div className=" flex" >
-          
+          <div className=" flex items-center gap-3 mt-8">
+            <div className="px-4 py-2 rounded-full  border-white/20 bg-gradient-to-b from-[#ddd] to-[#afafaf] flex items-center justify-center">
+              <p className="text-sm text-black  font-Poppins">
+                Getting Started
+              </p>
+            </div>
+            <div className="px-4 py-2 rounded-full border border-white/20 bg-[#262626] flex items-center justify-center">
+              <p className="text-sm text-white/50  font-Poppins">
+                Start on github
+              </p>
+            </div>
+          </div>
+
+          <div className=" h-96 w-full bg-[#262626] border-4 border-[#4c4c4c] rounded-md absolute ml-10 mt-10 object-contain">
+            <Image
+              src={`/dashboard.png`}
+              height={1500}
+              width={1500}
+              alt="image"
+            />
+          </div>
         </div>
+        <div>
+          <Image
+            className=" h-full object-cover w-full absolute  bottom-0 z-10 "
+            src={`/bg.svg`}
+            height={1500}
+            width={1500}
+            alt="image"
+          />
         </div>
-      <div>
-        <Image className="w-auto absolute  bottom-0 z-10 " src={`/bg.svg`} height={1500} width={1500} alt="image" />
-      </div>
       </div>
     </>
   );
--- components/shared/landingPage/landingPage.tsx
+++ components/shared/landingPage/landingPage.tsx
@@ -9,14 +9,22 @@ const LandingPage = () => {
   const darkmode = useSelector((state: any) => state.window.darkMode);
   return (
     <div className={`${darkmode ? "dark" : ""}`}>
-      <div className=" w-full min-h-screen h-screen bg-[#050505] dark:bg-[#050505] ">
-        <div className=" w-full sticky top-0">
+      <div style={{
+         scrollbarWidth: "none",
+         msOverflowStyle: "none",
+         WebkitOverflowScrolling: "touch",
+      }} className=" w-full min-h-screen h-screen overflow-scroll bg-[#050505] dark:bg-[#050505] ">
+        <div className=" w-full sticky top-0 z-50">
           <NavBar />
         </div>
 
-        <div className=" w-auto px-40 mt-10 relative" >
-          <div className=" w-auto h-[900px] border relative ">
+        <div className=" w-auto px-52 mt-10 relative z-20" >
+          <div className=" w-auto min-h-[1600px] border relative ">
             <FirstFold />
+            <div className=" w-full h-56 border-b" >
+
+            </div>
+            
           </div>
         </div>
       </div>
