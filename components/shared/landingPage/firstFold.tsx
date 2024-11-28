6f17ea97589337f457740c5471093619692b2c68
voiiddxx <ursfan8102003@gmail.com>
2024-11-28 22:20:35 +0530

    landing page done

--- components/shared/landingPage/firstFold.tsx
+++ components/shared/landingPage/firstFold.tsx
@@ -10,39 +10,43 @@ const FirstFold = () => {
 
   return (
     <>
-      <div className="h-[700px] overflow-hidden w-full border-b border-[#262626] relative">
+      <div className="h-[830px] overflow-hidden w-full border-b border-[#262626] relative">
         <div className=" h-full w-full relative z-50 md:p-16 p-8">
-          <h1 className="text-white/80 md:text-4xl text-4xl font-mono  text-center md:text-left font-medium  ">
+          <h1 className="text-white md:text-4xl text-4xl font-mono  text-center md:text-left font-medium  ">
             Automate your github workflows{" "}
           </h1>
-          <h1 className="text-white/80 md:text-4xl text-xl mt-2 font-mono  font-medium hidden md:inline-block    ">
+          <h1 className="text-white md:text-4xl text-xl mt-2 font-mono  font-medium  hidden md:inline-block    ">
             with less effort and ease
           </h1>
           <p className="text-white/50 font-Poppins md:text-[18px] text-base text-center md:text-left md:mt-8 mt-6">
-          Simplify your development workflow by automating commit and PR scheduling
+            Simplify your development workflow by automating commit and PR
+            scheduling
             <span className="hidden md:inline-block mt-1">
-            saving time, boosting productivity, and focusing on coding brilliance.
+              saving time, boosting productivity, and focusing on coding
+              brilliance.
             </span>
-           
           </p>
 
           <div className=" flex items-center gap-3 mt-8 justify-center md:justify-start">
-            <Link href={GITHUB_AUTH_URL}>
-            <div className="px-4 py-2 rounded-full  border-white/20 bg-gradient-to-b from-[#ddd] to-[#afafaf] flex items-center justify-center">
-              <p className="text-[12px] text-black  font-Poppins">
-                Comming Soon
-              </p>
-            </div></Link>
-            <div className="px-4 py-2 rounded-full border border-white/20 bg-[#262626] flex items-center justify-center">
-              <p className="text-[12px] text-white/50  font-Poppins">
-                Star on github
-              </p>
-            </div>
+            <Link target="_blank" href={`https://github.com/voiiddxx`}>
+              <div className="px-4 py-2 rounded-full  border-white/20 bg-gradient-to-b from-[#ddd] to-[#afafaf] flex items-center justify-center">
+                <p className="text-[12px] text-black  font-Poppins font-medium">
+                  Comming Soon
+                </p>
+              </div>
+            </Link>
+            <Link target="_blank" href={`https://github.com/voiiddxx`} >
+              <div className="px-4 py-2 rounded-full border border-white/20 bg-[#262626] flex items-center justify-center">
+                <p className="text-[12px] text-white/50  font-Poppins">
+                  Star on github
+                </p>
+              </div>
+            </Link>
           </div>
 
-          <div className=" h-72 md:h-96 w-full bg-[#262626] border-4 border-[#4c4c4c] rounded-md absolute ml-10 mt-10 object-contain">
+          <div className=" h-72 md:h-[500px] w-full bg-[#262626] border-4 border-[#4c4c4c] rounded-md absolute ml-10 mt-10 object-contain">
             <Image
-            className="h-full w-full object-cover md:object-top"
+              className="h-full w-full object-cover md:object-top"
               src={`/dashboard.png`}
               height={1500}
               width={1500}
--- components/shared/landingPage/navbar.tsx
+++ components/shared/landingPage/navbar.tsx
@@ -6,8 +6,10 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { GithubIcon } from "../dashboard/sideBar";
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 const NavBar = () => {
+  
   const dispatch = useDispatch();
   const darkMode = useSelector((state: any) => state.window.darkMode);
 
@@ -17,10 +19,7 @@ const NavBar = () => {
       <div className=" h-12 w-full flex items-center justify-between border border-[#262626] rounded-2xl px-4 bg-black bg-opacity-50 backdrop-blur-md">
         <div className=" flex gap-3 items-center">
           <div className="flex gap-1 items-center">
-           
-            <p className="text-sm font-mono  font-medium text-white/90">
-              Flow
-            </p>
+            <p className="text-sm font-mono  font-medium text-white/90">Flow</p>
           </div>
           <div className="md:flex gap-4 ml-6 hidden ">
             {options.map((option, index) => (
@@ -37,19 +36,28 @@ const NavBar = () => {
           </div>
         </div>
 
-        <div className=" flex gap-2 items-center" >
-            <div className="h-[35px] md:w-64 px-2 border bg-gradient-to-r from-[#161616] to-[#161616] bg-opacity-45 rounded-full border-zinc-800 hidden md:flex items-center justify-between" >
-            <div className="flex items-center gap-1" >
+        <div className=" flex gap-2 items-center">
+          <div className="h-[35px] md:w-64 px-2 border bg-gradient-to-r from-[#161616] to-[#161616] bg-opacity-45 rounded-full border-zinc-800 hidden md:flex items-center justify-between">
+            <div className="flex items-center gap-1">
               <Search size={20} className="text-white/50" />
-              <p className="text-white/60 font-normal text-sm" >Search</p>
+              <p className="text-white/60 font-normal text-sm">Search</p>
             </div>
 
-            <div className=" md:flex items-center hidden  gap-2" >
-              <p className="bg-black text-white/50 text-xs font-Poppins px-1 py-1 border rounded-md border-zinc-700" >Ctrl</p>
-              <p className="bg-black text-white/50 text-xs font-Poppins px-2 py-1 border rounded-md border-zinc-700" >K</p>
+            <div className=" md:flex items-center hidden  gap-2">
+              <p className="bg-black text-white/50 text-xs font-Poppins px-1 py-1 border rounded-md border-zinc-700">
+                Ctrl
+              </p>
+              <p className="bg-black text-white/50 text-xs font-Poppins px-2 py-1 border rounded-md border-zinc-700">
+                K
+              </p>
             </div>
-            </div>
-            <GitHubLogoIcon className="text-white/50 size-6" />
+          </div>
+          <Link target="_blank" href={`https://github.com/voiiddxx`}>
+            <GitHubLogoIcon
+              onClick={() => {}}
+              className="text-white/50 size-6"
+            />
+          </Link>
         </div>
       </div>
     </div>
