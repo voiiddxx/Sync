d5864cfe00efb06374ee6649afeab6c1b52696bd
voiiddxx <ursfan8102003@gmail.com>
2024-11-28 22:47:14 +0530

    lp done

--- components/shared/landingPage/firstFold.tsx
+++ components/shared/landingPage/firstFold.tsx
@@ -10,17 +10,17 @@ const FirstFold = () => {
 
   return (
     <>
-      <div className="h-[830px] overflow-hidden w-full border-b border-[#262626] relative">
-        <div className=" h-full w-full relative z-50 md:p-16 p-8">
-          <h1 className="text-white md:text-4xl text-4xl font-mono  text-center md:text-left font-medium  ">
+      <div className="md:h-[830px] h-[550px] overflow-hidden w-full border-b border-[#262626] relative">
+        <div className=" h-full w-full relative z-50 md:p-16 p-6 pt-10">
+          <h1 className="text-white md:text-4xl text-4xl font-mono  text-center md:text-left font-semibold  ">
             Automate your github workflows{" "}
           </h1>
           <h1 className="text-white md:text-4xl text-xl mt-2 font-mono  font-medium  hidden md:inline-block    ">
             with less effort and ease
           </h1>
           <p className="text-white/50 font-Poppins md:text-[18px] text-base text-center md:text-left md:mt-8 mt-6">
-            Simplify your development workflow by automating commit and PR
-            scheduling
+            Simplify your development workflow by automating <span className="text-white/80">commit</span>  and PR
+            <span className="text-white/80"> scheduling</span>
             <span className="hidden md:inline-block mt-1">
               saving time, boosting productivity, and focusing on coding
               brilliance
@@ -29,22 +29,22 @@ const FirstFold = () => {
 
           <div className=" flex items-center gap-3 mt-8 justify-center md:justify-start">
             <Link target="_blank" href={`https://github.com/voiiddxx`}>
-              <div className="px-4 py-2 rounded-full  border-white/20 bg-gradient-to-b from-[#ddd] to-[#afafaf] flex items-center justify-center">
+              <div className="px-5 py-3 rounded-full  border-white/20 bg-gradient-to-b from-[#ddd] to-[#727272] flex items-center justify-center">
                 <p className="text-[12px] text-black  font-Poppins font-medium">
                   Comming Soon
                 </p>
               </div>
             </Link>
             <Link target="_blank" href={`https://github.com/voiiddxx`} >
-              <div className="px-4 py-2 rounded-full border border-white/20 bg-[#262626] flex items-center justify-center">
-                <p className="text-[12px] text-white/50  font-Poppins">
-                  Star on github
+              <div className="px-5 py-3 rounded-full  bg-gradient-to-b from-[#262626] to-[#222222] flex items-center justify-center">
+                <p className="text-[12px] text-white/90 font-medium  font-Poppins">
+                  Star on Github
                 </p>
               </div>
             </Link>
           </div>
 
-          <div className=" h-72 md:h-[500px] w-full bg-[#262626] border-4 border-[#4c4c4c] rounded-md absolute ml-10 mt-10 object-contain">
+          <div className=" h-72 md:h-[500px] w-full bg-[#262626] border-4 border-[#4c4c4c] rounded-md absolute md:ml-10 mt-10 object-contain">
             <Image
               className="h-full w-full object-cover md:object-top"
               src={`/dashboard.png`}
